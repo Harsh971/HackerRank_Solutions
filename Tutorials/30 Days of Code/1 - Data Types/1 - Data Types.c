1e5103f46ab6ca547afeca675186f9703c58e611
Harsh Thakkar <65072835+Harsh971@users.noreply.github.com>
2022-05-12 02:52:31 +0530

    Solution in C Language

---
+++ Tutorials/30 Days of Code/1 - Data Types/1 - Data Types.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+    int i = 4;
+    double d = 4.0;
+    char s[] = "HackerRank ";
+
+    // Declare second integer, double, and String variables.
+    int second_integer;
+    double second_double;
+    char second_string[100];
+    
+    // Read and save an integer, double, and String to your variables.
+    scanf("%d\n",&second_integer);
+    scanf("%lf\n",&second_double);
+    fgets(second_string, 100, stdin);
+
+    // Print the sum of both integer variables on a new line.
+    printf("%d\n",i+second_integer);
+    
+    // Print the sum of the double variables on a new line.
+    printf("%.1lf\n",d+second_double);
+    
+    printf("%s%s",s,second_string);
+
+    return 0;
+}
