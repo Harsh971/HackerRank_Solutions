65732e285faf685be6b49f9a2dcd5c535423e586
Harsh Thakkar <65072835+Harsh971@users.noreply.github.com>
2022-05-20 18:45:00 +0530

    Solution Using JAVA

---
+++ Tutorials/30 Days of Code/8 - Dictionaries and Maps/8 - Dictionaries and Maps.java
@@ -0,0 +1,27 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+         Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        Map<String, Integer> phoneBook = new HashMap<String, Integer>();
+        for(int i = 0; i < n; i++){
+            String name = in.next();
+            int phone = in.nextInt();
+            phoneBook.put(name, phone);
+
+        }
+        while(in.hasNext()){
+            String s = in.next();
+            if(phoneBook.containsKey(s))
+                System.out.println(s + "=" + phoneBook.get(s));
+            else
+                System.out.println("Not found");
+
+
+        }
+        in.close();
+    }
+}
