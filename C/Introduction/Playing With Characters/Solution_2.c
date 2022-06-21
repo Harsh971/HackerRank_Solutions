5a30e6c42e33361c42ec43a6110fb70b4301dc46
Harsh Thakkar <65072835+Harsh971@users.noreply.github.com>
2022-06-22 00:26:00 +0530

    Create Solution_2.c

---
+++ C/Introduction/Playing With Characters/Solution_2.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+    char ch;
+    char s[50], sen[50];
+    scanf("%c", &ch);
+    scanf("%s", s);
+    scanf("\n");
+    scanf("%[^\n]%*c", sen);
+    printf("%c\n", ch);
+    printf("%s\n", s);
+    printf("%s\n", sen);
+    return 0;
+}
