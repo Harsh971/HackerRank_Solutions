98c9d694f0849988dc75e5c7c3be95cb9fd4dbf9
Harsh Thakkar <65072835+Harsh971@users.noreply.github.com>
2022-07-19 17:52:47 +0530

    Create Solution.c

---
+++ C/Arrays and Strings/Printing Tokens/Solution.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main()
+{
+    char *s;
+    s = malloc(1024 * sizeof(char));
+    scanf("%[^\n]", s);
+    s = realloc(s, strlen(s) + 1);
+    int len = strlen(s);
+    for(int i = 0; i < len; i++) {
+        if(s[i] == ' ') {
+            printf("\n");
+        }
+        else {
+            printf("%c", s[i]);
+        }
+    }
+    free(s);
+    return 0;
+}
