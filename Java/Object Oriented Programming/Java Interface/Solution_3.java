6fc2b551b0ef1b868595e804bedf9a7ff61caca4
Harsh Thakkar <65072835+Harsh971@users.noreply.github.com>
2022-08-10 11:00:57 +0530

    Add files via upload

---
+++ Java/Object Oriented Programming/Java Interface/Solution_3.java
@@ -0,0 +1,41 @@
+import java.util.*;
+interface AdvancedArithmetic {
+ public abstract int divisorSum(int n);
+}
+
+//Solution starts
+class MyCalculator implements AdvancedArithmetic {
+ public int divisor_sum(int n) {
+  int ans = 0;
+  for (int i = 1; i <= n; i++) {
+   if (n % i == 0) ans += i;
+  }
+  return ans;
+ }
+}
+//Solution ends
+
+
+class Solution {
+
+ public static void main(String[] argh) {
+   MyCalculator my_calculator = new MyCalculator();
+   System.out.print("I implemented: ");
+   ImplementedInterfaceNames(my_calculator);
+   Scanner sc = new Scanner(System.in);
+   int n = sc.nextInt();
+   System.out.print(my_calculator.divisorSum(n) + "\n");
+
+  }
+  /*
+   *  ImplementedInterfaceNames method takes an object and prints the name of the interfaces it implemented
+   */
+ static void ImplementedInterfaceNames(Object o) {
+
+  Class[] theInterfaces = o.getClass().getInterfaces();
+  for (int i = 0; i < theInterfaces.length; i++) {
+   String interfaceName = theInterfaces[i].getName();
+   System.out.println(interfaceName);
+  }
+ }
+}
