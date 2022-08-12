95bf8659201ecad1d5f32b5b034bed9e912b47ea
Harsh Thakkar <65072835+Harsh971@users.noreply.github.com>
2022-08-12 17:42:00 +0530

    Add files via upload

---
+++ Java/Data Structures/Java 1D Array/Solution_2.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+       
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+      int[] a = new int[n];
+        for(int i = 0 ; i < n; i++){
+          int val = scan.nextInt();
+            a[i] = val;
+      }
+        scan.close();
+
+        for (int i = 0; i < a.length; i++) {
+            System.out.println(a[i]);
+        }
+    }
+}
