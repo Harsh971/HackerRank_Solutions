c80560455ea2c85f82dcf7bb063302cfdfd29d1b
Harsh Thakkar <65072835+Harsh971@users.noreply.github.com>
2022-08-17 20:48:38 +0530

    Add files via upload

---
+++ Java/BigNumber/Java Primality Test/Solution.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution
+{
+    public static void main(String[] args)
+    {
+        try (Scanner scanner = new Scanner(System.in);)
+        {
+            System.out.println(scanner.nextBigInteger().isProbablePrime(100) ? "prime" : "not prime");
+        }
+    }
+}
